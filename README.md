72b0de7fe955ae6ad13e355ae2225497438b9af5
Insality <insality@gmail.com>
2024-03-09 13:45:58 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,202 @@
+![](media/logo.png)
+
+[![Github-sponsors](https://img.shields.io/badge/sponsor-30363D?style=for-the-badge&logo=GitHub-Sponsors&logoColor=#EA4AAA)](https://github.com/sponsors/insality) [![Ko-Fi](https://img.shields.io/badge/Ko--fi-F16061?style=for-the-badge&logo=ko-fi&logoColor=white)](https://ko-fi.com/insality) [![BuyMeACoffee](https://img.shields.io/badge/Buy%20Me%20a%20Coffee-ffdd00?style=for-the-badge&logo=buy-me-a-coffee&logoColor=black)](https://www.buymeacoffee.com/insality)
+
+[![GitHub release (latest by date)](https://img.shields.io/github/v/tag/insality/defold-tweener?style=for-the-badge&label=Release)](https://github.com/Insality/defold-tweener/tags)
+
+
+# Tweener
+
+**Tweener** - is a single file Lua module for the [Defold](https://defold.com/) game engine. It provides a way to handle manual tweening in your game.
+
+## Features
+
+- **Tweening**: Create tweens for any action your want.
+- **Easing Functions**: Provides a set of easing functions for different types of easings.
+- **Custom Update Frequency**: Option to define update frequency for the tween.
+- **Callbacks**: Callbacks for each tween update.
+
+## Setup
+
+### [Dependency](https://www.defold.com/manuals/libraries/)
+
+Open your `game.project` file and add the following line to the dependencies field under the project section:
+
+**[Tweener v1](https://github.com/Insality/defold-tweener/archive/refs/tags/1.zip)**
+
+```
+https://github.com/Insality/defold-tweener/archive/refs/tags/1.zip
+```
+
+Size: **5.47 KB**
+
+### Global Update Frequency
+
+Optionally, you can setup global default update freqency in your game.project. It's `60` by default.
+
+Add next `tweener` section to your `game.project` in text mode:
+
+```ini
+[tweener]
+update_frequency = 60
+```
+
+## API
+
+### Importing the Module
+
+To start using the Tweener module in your project, you first need to import it. This can be done with the following line of code:
+
+```lua
+local tweener = require("tweener.tweener")
+```
+
+### Functions
+
+The Tweener module provides two primary functions to work with tweens:
+
+**tweener.tween**
+---
+```lua
+tweener.tween(tween_function, from, to, time, callback, [dt])
+```
+
+This function initiates a tween operation immediately. Here's how to use it:
+
+- **Parameters:**
+  - `tween_function`: The tween function.
+  - `from`: The starting value of the tween.
+  - `to`: The ending value of the tween.
+  - `time`: The duration of the tween, in seconds.
+  - `callback`: A `function(value, is_final_call)` that gets called upon each update of the tween.
+  - `dt` (optional): The time interval for updating the tween, in seconds.
+
+- **Return Value:**
+  - `timer_id`: A timer id from `timer.delay` if you wish to cancel the tween.
+
+- **Usage Example:**
+
+```lua
+tweener.tween(go.EASING_OUTSINE, 0, 100, 1.5, function(value, is_final_call)
+	print("Tween value: " .. value)
+end)
+```
+
+
+**tweener.ease**
+---
+```lua
+tweener.ease(tween_function, from, to, time, time_elapsed)
+```
+
+This function calculates the value of the tween at a specific point in time, based on the easing function provided.
+
+- **Parameters:**
+  - `tween_function`: The tween function.
+  - `from`: The starting value of the tween.
+  - `to`: The ending value of the tween.
+  - `time`: The total duration of the tween, in seconds.
+  - `time_elapsed`: The elapsed time since the start of the tween, in seconds.
+
+- **Usage Example:**
+
+```lua
+local value = tweener.ease(tweener.inquad, 0, 100, 1.5, 0.75)
+print("The tween value at halfway point is: ", value)
+```
+
+These functions provide a flexible and powerful way to add animations and transitions to your projects, making them feel more dynamic and engaging. Enjoy animating with the **Tweener** module! *(Thanks, Mister ChatGPT)* 🙃
+
+
+## Tween Functions
+
+| Tweener Constant | Defold Constant | Easing |
+|------------------|-----------------|--------|
+| `tweener.linear` | `go.EASING_LINEAR` | <img src="media/easing_icons/linear.png" width="50" height="50"> |
+| `tweener.insine` | `go.EASING_INSINE` | <img src="media/easing_icons/insine.png" width="50" height="50"> |
+| `tweener.inquad` | `go.EASING_INQUAD` | <img src="media/easing_icons/inquad.png" width="50" height="50"> |
+| `tweener.incubic` | `go.EASING_INCUBIC` | <img src="media/easing_icons/incubic.png" width="50" height="50"> |
+| `tweener.incirc` | `go.EASING_INCIRC` | <img src="media/easing_icons/incirc.png" width="50" height="50"> |
+| `tweener.inquart` | `go.EASING_INQUART` | <img src="media/easing_icons/inquart.png" width="50" height="50"> |
+| `tweener.inquint` | `go.EASING_INQUINT` | <img src="media/easing_icons/inquint.png" width="50" height="50"> |
+| `tweener.inexpo` | `go.EASING_INEXPO` | <img src="media/easing_icons/inexpo.png" width="50" height="50"> |
+| `tweener.inback` | `go.EASING_INBACK` | <img src="media/easing_icons/inback.png" width="50" height="50"> |
+| `tweener.inelastic` | `go.EASING_INELASTIC` | <img src="media/easing_icons/inelastic.png" width="50" height="50"> |
+| `tweener.inbounce` | `go.EASING_INBOUNCE` | <img src="media/easing_icons/inbounce.png" width="50" height="50"> |
+| `tweener.outsine` | `go.EASING_OUTSINE` | <img src="media/easing_icons/outsine.png" width="50" height="50"> |
+| `tweener.outquad` | `go.EASING_OUTQUAD` | <img src="media/easing_icons/outquad.png" width="50" height="50"> |
+| `tweener.outcubic` | `go.EASING_OUTCUBIC` | <img src="media/easing_icons/outcubic.png" width="50" height="50"> |
+| `tweener.outcirc` | `go.EASING_OUTCIRC` | <img src="media/easing_icons/outcirc.png" width="50" height="50"> |
+| `tweener.outquart` | `go.EASING_OUTQUART` | <img src="media/easing_icons/outquart.png" width="50" height="50"> |
+| `tweener.outquint` | `go.EASING_OUTQUINT` | <img src="media/easing_icons/outquint.png" width="50" height="50"> |
+| `tweener.outexpo` | `go.EASING_OUTEXPO` | <img src="media/easing_icons/outexpo.png" width="50" height="50"> |
+| `tweener.outback` | `go.EASING_OUTBACK` | <img src="media/easing_icons/outback.png" width="50" height="50"> |
+| `tweener.outelastic` | `go.EASING_OUTELASTIC` | <img src="media/easing_icons/outelastic.png" width="50" height="50"> |
+| `tweener.outbounce` | `go.EASING_OUTBOUNCE` | <img src="media/easing_icons/outbounce.png" width="50" height="50"> |
+| `tweener.inoutsine` | `go.EASING_INOUTSINE` | <img src="media/easing_icons/inoutsine.png" width="50" height="50"> |
+| `tweener.inoutquad` | `go.EASING_INOUTQUAD` | <img src="media/easing_icons/inoutquad.png" width="50" height="50"> |
+| `tweener.inoutcubic` | `go.EASING_INOUTCUBIC` | <img src="media/easing_icons/inoutcubic.png" width="50" height="50"> |
+| `tweener.inoutcirc` | `go.EASING_INOUTCIRC` | <img src="media/easing_icons/inoutcirc.png" width="50" height="50"> |
+| `tweener.inoutquart` | `go.EASING_INOUTQUART` | <img src="media/easing_icons/inoutquart.png" width="50" height="50"> |
+| `tweener.inoutquint` | `go.EASING_INOUTQUINT` | <img src="media/easing_icons/inoutquint.png" width="50" height="50"> |
+| `tweener.inoutexpo` | `go.EASING_INOUTEXPO` | <img src="media/easing_icons/inoutexpo.png" width="50" height="50"> |
+| `tweener.inoutback` | `go.EASING_INOUTBACK` | <img src="media/easing_icons/inoutback.png" width="50" height="50"> |
+| `tweener.inoutelastic` | `go.EASING_INOUTELASTIC` | <img src="media/easing_icons/inoutelastic.png" width="50" height="50"> |
+| `tweener.inoutbounce` | `go.EASING_INOUTBOUNCE` | <img src="media/easing_icons/inoutbounce.png" width="50" height="50"> |
+| `tweener.outinsine` | `go.EASING_OUTINSINE` | <img src="media/easing_icons/outinsine.png" width="50" height="50"> |
+| `tweener.outinquad` | `go.EASING_OUTINQUAD` | <img src="media/easing_icons/outinquad.png" width="50" height="50"> |
+| `tweener.outincubic` | `go.EASING_OUTINCUBIC` | <img src="media/easing_icons/outincubic.png" width="50" height="50"> |
+| `tweener.outincirc` | `go.EASING_OUTINCIRC` | <img src="media/easing_icons/outincirc.png" width="50" height="50"> |
+| `tweener.outinquart` | `go.EASING_OUTINQUART` | <img src="media/easing_icons/outinquart.png" width="50" height="50"> |
+| `tweener.outinquint` | `go.EASING_OUTINQUINT` | <img src="media/easing_icons/outinquint.png" width="50" height="50"> |
+| `tweener.outinexpo` | `go.EASING_OUTINEXPO` | <img src="media/easing_icons/outinexpo.png" width="50" height="50"> |
+| `tweener.outinback` | `go.EASING_OUTINBACK` | <img src="media/easing_icons/outinback.png" width="50" height="50"> |
+| `tweener.outinelastic` | `go.EASING_OUTINELASTIC` | <img src="media/easing_icons/outinelastic.png" width="50" height="50"> |
+| `tweener.outinbounce` | `go.EASING_OUTINBOUNCE` | <img src="media/easing_icons/outinbounce.png" width="50" height="50"> |
+
+
+## Usage
+
+You can use Tweener to animate scoring text, for example:
+
+<img src="media/tween_text.gif" width="300">
+
+This animation can be created using the following code:
+
+```lua
+tweener.tween(gui.EASING_OUTCIRC, 0, 9999, 2.4, function(value, is_final_call)
+	gui.set_text(text_score, "Score: " .. math.floor(value))
+
+	if is_final_call then
+		gui.set_scale(text_score, vmath.vector3(1.25, 1.25, 1))
+		gui.animate(text_score, "scale", vmath.vector3(1, 1, 1), gui.EASING_OUTBOUNCE, 0.5)
+	end
+end)
+```
+
+You can obtain the value of the tween at any point in time with the `tweener.ease` function:
+
+```lua
+tweener.ease(gui.EASING_OUTSINE, 0, 100, 1, 0)    -- Returns 0
+tweener.ease(gui.EASING_OUTSINE, 0, 100, 1, 0.25) -- Returns 38.268343236509
+tweener.ease(gui.EASING_OUTSINE, 0, 100, 1, 0.5)  -- Returns 70.710678118655
+tweener.ease(gui.EASING_OUTSINE, 0, 100, 1, 0.75) -- Returns 92.387953251129
+tweener.ease(gui.EASING_OUTSINE, 0, 100, 1, 1)    -- Returns 100
+```
+
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+
+## Issues and suggestions
+
+If you have any issues, questions or suggestions please [create an issue](https://github.com/Insality/defold-tweener/issues).
+
+
+## ❤️ Support project ❤️
+
+Your donation helps me stay engaged in creating valuable projects for **Defold**. If you appreciate what I'm doing, please consider supporting me!
+
+[![Github-sponsors](https://img.shields.io/badge/sponsor-30363D?style=for-the-badge&logo=GitHub-Sponsors&logoColor=#EA4AAA)](https://github.com/sponsors/insality) [![Ko-Fi](https://img.shields.io/badge/Ko--fi-F16061?style=for-the-badge&logo=ko-fi&logoColor=white)](https://ko-fi.com/insality) [![BuyMeACoffee](https://img.shields.io/badge/Buy%20Me%20a%20Coffee-ffdd00?style=for-the-badge&logo=buy-me-a-coffee&logoColor=black)](https://www.buymeacoffee.com/insality)
---
+++ tweener/tweener.lua
@@ -0,0 +1,536 @@
+---@alias easing_function (fun(current: number, from: number, to: number, time: number): number)|constant
+
+local UPDATE_FREQUENCY = sys.get_config_int("tweener.UPDATE_FREQUENCY", 60)
+
+local M = {}
+
+---@param easing_function easing_function
+---@param from number
+---@param to number
+---@param time number
+---@param callback fun(value: number, is_end: boolean)
+---@param update_delta_time number|nil @Default is 1/60, the time between updates
+---@return hash @The created timer id
+function M.tween(easing_function, from, to, time, callback, update_delta_time)
+	update_delta_time = update_delta_time or (1 / UPDATE_FREQUENCY)
+	easing_function = M.DEFOLD_EASINGS[easing_function] or easing_function
+
+	local time_elapsed = 0
+	local timer_id = timer.delay(update_delta_time, true, function(_, handle, dt)
+		time_elapsed = time_elapsed + dt
+
+		if time_elapsed >= time then
+			timer.cancel(handle)
+			callback(easing_function(time, from, to - from, time), true)
+			return
+		end
+
+		callback(easing_function(time_elapsed, from, to - from, time), false)
+	end)
+
+	return timer_id
+end
+
+
+---@param easing_function easing_function
+---@param from number
+---@param to number
+---@param time number
+---@param time_elapsed number @current time [0 .. t - from]
+---@return number @The result of easing
+function M.ease(easing_function, from, to, time, time_elapsed)
+	if time == 0 then
+		return to
+	end
+
+	easing_function = M.DEFOLD_EASINGS[easing_function] or easing_function
+	return easing_function(time_elapsed, from, to - from, time)
+end
+
+
+--
+-- Adapted from
+-- Tweener's easing functions (Penner's Easing Equations)
+-- and http://code.google.com/p/tweener/ (jstweener javascript version)
+--
+
+--[[
+Disclaimer for Robert Penner's Easing Equations license:
+
+TERMS OF USE - EASING EQUATIONS
+
+Open source under the BSD License.
+
+Copyright © 2001 Robert Penner
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+	* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+	* Neither the name of the author nor the names of contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUsineSS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+]]
+
+-- For all easing functions:
+-- t = elapsed time
+-- b = begin
+-- c = change == ending - beginning
+-- d = duration (total time)
+
+local pi = math.pi
+local abs = math.abs
+local cos = math.cos
+local pow = math.pow
+local sin = math.sin
+local sqrt = math.sqrt
+local asin	= math.asin
+
+local function linear(t, b, c, d)
+	return c * t / d + b
+end
+
+local function inquad(t, b, c, d)
+	t = t / d
+	return c * pow(t, 2) + b
+end
+
+local function outquad(t, b, c, d)
+	t = t / d
+	return -c * t * (t - 2) + b
+end
+
+local function inoutquad(t, b, c, d)
+	t = t / d * 2
+	if t < 1 then
+		return c / 2 * pow(t, 2) + b
+	else
+		return -c / 2 * ((t - 1) * (t - 3) - 1) + b
+	end
+end
+
+local function outinquad(t, b, c, d)
+	if t < d / 2 then
+		return outquad (t * 2, b, c / 2, d)
+	else
+		return inquad((t * 2) - d, b + c / 2, c / 2, d)
+	end
+end
+
+local function incubic(t, b, c, d)
+	t = t / d
+	return c * pow(t, 3) + b
+end
+
+local function outcubic(t, b, c, d)
+	t = t / d - 1
+	return c * (pow(t, 3) + 1) + b
+end
+
+local function inoutcubic(t, b, c, d)
+	t = t / d * 2
+	if t < 1 then
+		return c / 2 * t * t * t + b
+	else
+		t = t - 2
+		return c / 2 * (t * t * t + 2) + b
+	end
+end
+
+local function outincubic(t, b, c, d)
+	if t < d / 2 then
+		return outcubic(t * 2, b, c / 2, d)
+	else
+		return incubic((t * 2) - d, b + c / 2, c / 2, d)
+	end
+end
+
+local function inquart(t, b, c, d)
+	t = t / d
+	return c * pow(t, 4) + b
+end
+
+local function outquart(t, b, c, d)
+	t = t / d - 1
+	return -c * (pow(t, 4) - 1) + b
+end
+
+local function inoutquart(t, b, c, d)
+	t = t / d * 2
+	if t < 1 then
+		return c / 2 * pow(t, 4) + b
+	else
+		t = t - 2
+		return -c / 2 * (pow(t, 4) - 2) + b
+	end
+end
+
+local function outinquart(t, b, c, d)
+	if t < d / 2 then
+		return outquart(t * 2, b, c / 2, d)
+	else
+		return inquart((t * 2) - d, b + c / 2, c / 2, d)
+	end
+end
+
+local function inquint(t, b, c, d)
+	t = t / d
+	return c * pow(t, 5) + b
+end
+
+local function outquint(t, b, c, d)
+	t = t / d - 1
+	return c * (pow(t, 5) + 1) + b
+end
+
+local function inoutquint(t, b, c, d)
+	t = t / d * 2
+	if t < 1 then
+		return c / 2 * pow(t, 5) + b
+	else
+		t = t - 2
+		return c / 2 * (pow(t, 5) + 2) + b
+	end
+end
+
+local function outinquint(t, b, c, d)
+	if t < d / 2 then
+		return outquint(t * 2, b, c / 2, d)
+	else
+		return inquint((t * 2) - d, b + c / 2, c / 2, d)
+	end
+end
+
+local function insine(t, b, c, d)
+	return -c * cos(t / d * (pi / 2)) + c + b
+end
+
+local function outsine(t, b, c, d)
+	return c * sin(t / d * (pi / 2)) + b
+end
+
+local function inoutsine(t, b, c, d)
+	return -c / 2 * (cos(pi * t / d) - 1) + b
+end
+
+local function outinsine(t, b, c, d)
+	if t < d / 2 then
+		return outsine(t * 2, b, c / 2, d)
+	else
+		return insine((t * 2) -d, b + c / 2, c / 2, d)
+	end
+end
+
+local function inexpo(t, b, c, d)
+	if t == 0 then
+		return b
+	else
+		return c * pow(2, 10 * (t / d - 1)) + b - c * 0.001
+	end
+end
+
+local function outexpo(t, b, c, d)
+	local epsilon = 0.00001 -- tolerance
+	if math.abs(t - d) < epsilon then
+		return b + c
+	end
+	return c * 1.001 * (-pow(2, -10 * t / d) + 1) + b
+end
+
+local function inoutexpo(t, b, c, d)
+	if t == 0 then return b end
+	if t == d then return b + c end
+
+	t = t / d * 2
+	if t < 1 then
+		return c / 2 * pow(2, 10 * (t - 1)) + b - c * 0.0005
+	else
+		t = t - 1
+		return c / 2 * 1.0005 * (-pow(2, -10 * t) + 2) + b
+	end
+end
+
+local function outinexpo(t, b, c, d)
+	if t < d / 2 then
+		return outexpo(t * 2, b, c / 2, d)
+	else
+		return inexpo((t * 2) - d, b + c / 2, c / 2, d)
+	end
+end
+
+local function incirc(t, b, c, d)
+	t = t / d
+	return(-c * (sqrt(1 - pow(t, 2)) - 1) + b)
+end
+
+local function outcirc(t, b, c, d)
+	t = t / d - 1
+	return(c * sqrt(1 - pow(t, 2)) + b)
+end
+
+local function inoutcirc(t, b, c, d)
+	t = t / d * 2
+	if t < 1 then
+		return -c / 2 * (sqrt(1 - t * t) - 1) + b
+	else
+		t = t - 2
+		return c / 2 * (sqrt(1 - t * t) + 1) + b
+	end
+end
+
+local function outincirc(t, b, c, d)
+	if t < d / 2 then
+		return outcirc(t * 2, b, c / 2, d)
+	else
+		return incirc((t * 2) - d, b + c / 2, c / 2, d)
+	end
+end
+
+local function inelastic(t, b, c, d, a, p)
+	if t == 0 then return b end
+
+	t = t / d
+	if t == 1 then
+		return b + c
+	end
+
+	if not p then
+		p = d * 0.
+	end
+
+	local s
+	if not a or a < abs(c) then
+		a = c
+		s = p / 4
+	else
+		s = p / (2 * pi) * asin(c/a)
+	end
+
+	t = t - 1
+	return -(a * pow(2, 10 * t) * sin((t * d - s) * (2 * pi) / p)) + b
+end
+
+-- a: amplitud
+-- p: period
+local function outelastic(t, b, c, d, a, p)
+	if t == 0 then
+		return b
+	end
+
+	t = t / d
+	if t == 1 then
+		return b + c
+	end
+
+	if not p then
+		p = d * 0.3
+	end
+
+	local s
+	if not a or a < abs(c) then
+		a = c
+		s = p / 4
+	else
+		s = p / (2 * pi) * asin(c/a)
+	end
+
+	return a * pow(2, -10 * t) * sin((t * d - s) * (2 * pi) / p) + c + b
+end
+
+-- p = period
+-- a = amplitud
+local function inoutelastic(t, b, c, d, a, p)
+	if t == 0 then return b end
+
+	t = t / d * 2
+
+	if t == 2 then return b + c end
+
+	if not p then p = d * (0.3 * 1.5) end
+	if not a then a = 0 end
+
+	local s
+
+	if not a or a < abs(c) then
+		a = c
+		s = p / 4
+	else
+		s = p / (2 * pi) * asin(c / a)
+	end
+
+	if t < 1 then
+		t = t - 1
+		return -0.5 * (a * pow(2, 10 * t) * sin((t * d - s) * (2 * pi) / p)) + b
+	else
+		t = t - 1
+		return a * pow(2, -10 * t) * sin((t * d - s) * (2 * pi) / p ) * 0.5 + c + b
+	end
+end
+
+-- a: amplitud
+-- p: period
+local function outinelastic(t, b, c, d, a, p)
+	if t < d / 2 then
+		return outelastic(t * 2, b, c / 2, d, a, p)
+	else
+		return inelastic((t * 2) - d, b + c / 2, c / 2, d, a, p)
+	end
+end
+
+local function inback(t, b, c, d, s)
+	if not s then s = 1.70158 end
+	t = t / d
+	return c * t * t * ((s + 1) * t - s) + b
+end
+
+local function outback(t, b, c, d, s)
+	if not s then s = 1.70158 end
+	t = t / d - 1
+	return c * (t * t * ((s + 1) * t + s) + 1) + b
+end
+
+local function inoutback(t, b, c, d, s)
+	if not s then s = 1.70158 end
+	s = s * 1.525
+	t = t / d * 2
+	if t < 1 then
+		return c / 2 * (t * t * ((s + 1) * t - s)) + b
+	else
+		t = t - 2
+		return c / 2 * (t * t * ((s + 1) * t + s) + 2) + b
+	end
+end
+
+local function outinback(t, b, c, d, s)
+	if t < d / 2 then
+		return outback(t * 2, b, c / 2, d, s)
+	else
+		return inback((t * 2) - d, b + c / 2, c / 2, d, s)
+	end
+end
+
+local function outbounce(t, b, c, d)
+	t = t / d
+	if t < 1 / 2.75 then
+		return c * (7.5625 * t * t) + b
+	elseif t < 2 / 2.75 then
+		t = t - (1.5 / 2.75)
+		return c * (7.5625 * t * t + 0.75) + b
+	elseif t < 2.5 / 2.75 then
+		t = t - (2.25 / 2.75)
+		return c * (7.5625 * t * t + 0.9375) + b
+	else
+		t = t - (2.625 / 2.75)
+		return c * (7.5625 * t * t + 0.984375) + b
+	end
+end
+
+local function inbounce(t, b, c, d)
+	return c - outbounce(d - t, 0, c, d) + b
+end
+
+local function inoutbounce(t, b, c, d)
+	if t < d / 2 then
+		return inbounce(t * 2, 0, c, d) * 0.5 + b
+	else
+		return outbounce(t * 2 - d, 0, c, d) * 0.5 + c * .5 + b
+	end
+end
+
+local function outinbounce(t, b, c, d)
+	if t < d / 2 then
+		return outbounce(t * 2, b, c / 2, d)
+	else
+		return inbounce((t * 2) - d, b + c / 2, c / 2, d)
+	end
+end
+
+M.linear = linear
+M.inquad = inquad
+M.outquad = outquad
+M.inoutquad = inoutquad
+M.outinquad = outinquad
+M.incubic = incubic
+M.outcubic = outcubic
+M.inoutcubic = inoutcubic
+M.outincubic = outincubic
+M.inquart = inquart
+M.outquart = outquart
+M.inoutquart = inoutquart
+M.outinquart = outinquart
+M.inquint = inquint
+M.outquint = outquint
+M.inoutquint = inoutquint
+M.outinquint = outinquint
+M.insine = insine
+M.outsine = outsine
+M.inoutsine = inoutsine
+M.outinsine = outinsine
+M.inexpo = inexpo
+M.outexpo = outexpo
+M.inoutexpo = inoutexpo
+M.outinexpo = outinexpo
+M.incirc = incirc
+M.outcirc = outcirc
+M.inoutcirc = inoutcirc
+M.outincirc = outincirc
+M.inelastic = inelastic
+M.outelastic = outelastic
+M.inoutelastic = inoutelastic
+M.outinelastic = outinelastic
+M.inback = inback
+M.outback = outback
+M.inoutback = inoutback
+M.outinback = outinback
+M.inbounce = inbounce
+M.outbounce = outbounce
+M.inoutbounce = inoutbounce
+M.outinbounce = outinbounce
+
+-- The constants from go.EASING_* and gui.EASING_* are the same
+M.DEFOLD_EASINGS = {
+	[go.EASING_INBACK] = inback,
+	[go.EASING_INBOUNCE] = inbounce,
+	[go.EASING_INCIRC] = incirc,
+	[go.EASING_INCUBIC] = incubic,
+	[go.EASING_INELASTIC] = inelastic,
+	[go.EASING_INEXPO] = inexpo,
+	[go.EASING_INOUTBACK] = inoutback,
+	[go.EASING_INOUTBOUNCE] = inoutbounce,
+	[go.EASING_INOUTCIRC] = inoutcirc,
+	[go.EASING_INOUTCUBIC] = inoutcubic,
+	[go.EASING_INOUTELASTIC] = inoutelastic,
+	[go.EASING_INOUTEXPO] = inoutexpo,
+	[go.EASING_INOUTQUAD] = inoutquad,
+	[go.EASING_INOUTQUART] = inoutquart,
+	[go.EASING_INOUTQUINT] = inoutquint,
+	[go.EASING_INOUTSINE] = inoutsine,
+	[go.EASING_INQUAD] = inquad,
+	[go.EASING_INQUART] = inquart,
+	[go.EASING_INQUINT] = inquint,
+	[go.EASING_INSINE] = insine,
+	[go.EASING_LINEAR] = linear,
+	[go.EASING_OUTBACK] = outback,
+	[go.EASING_OUTBOUNCE] = outbounce,
+	[go.EASING_OUTCIRC] = outcirc,
+	[go.EASING_OUTCUBIC] = outcubic,
+	[go.EASING_OUTELASTIC] = outelastic,
+	[go.EASING_OUTEXPO] = outexpo,
+	[go.EASING_OUTINBACK] = outinback,
+	[go.EASING_OUTINBOUNCE] = outinbounce,
+	[go.EASING_OUTINCIRC] = outincirc,
+	[go.EASING_OUTINCUBIC] = outincubic,
+	[go.EASING_OUTINELASTIC] = outinelastic,
+	[go.EASING_OUTINEXPO] = outinexpo,
+	[go.EASING_OUTINQUAD] = outinquad,
+	[go.EASING_OUTINQUART] = outinquart,
+	[go.EASING_OUTINQUINT] = outinquint,
+	[go.EASING_OUTINSINE] = outinsine,
+	[go.EASING_OUTQUAD] = outquad,
+	[go.EASING_OUTQUART] = outquart,
+	[go.EASING_OUTQUINT] = outquint,
+	[go.EASING_OUTSINE] = outsine
+}
+
+return M
